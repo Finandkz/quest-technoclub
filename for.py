557bf43935d5531318b167a5872a9231c7b8f5ec
Finand kz <kanahayaz22@gmail.com>
2024-10-06 15:50:48 +0700

    Add files via upload

---
+++ for.py
@@ -0,0 +1,20 @@
+"""
+Diambang Batas Pengulangan
+
+Naila bingung kode miliknya tidak bisa berhenti ketika menggunakan while loop.
+harapan dari naila adalah ketika output sudah mencapai 100, maka program akan berhenti.
+tapi sepertinya kode yang dia buat tidak berjalan sesuai harapan.
+bantu naila untuk menyelesaikan masalahnya.
+"""
+
+
+i = 0
+while (True):
+    if i >= 101:
+        break
+        
+    print('i')
+    
+    i += 1
+
+#udah
